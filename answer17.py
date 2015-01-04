e806e30dc3ea417eb73f459af180da981c98a37c
Ozan Topal <ozantopal@users.noreply.github.com>
2015-01-04 17:53:22 +0200

    Added a few new solutions

---
+++ answer17.py
@@ -0,0 +1,60 @@
+"""
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+"""	
+
+words = {
+    1 : 'one',
+    2 : 'two',
+    3 : 'three',
+    4 : 'four',
+    5 : 'five',
+    6 : 'six',
+    7 : 'seven',
+    8 : 'eight',
+    9 : 'nine',
+    10 : 'ten',
+    11 : 'eleven',
+    12 : 'twelve',
+    13 : 'thirteen',
+    14 : 'fourteen',
+    15 : 'fifteen',
+    16 : 'sixteen',
+    17 : 'seventeen',
+    18 : 'eighteen',
+    19 : 'nineteen',
+    20 : 'twenty',
+    30 : 'thirty',
+    40 : 'forty',
+    50 : 'fifty',
+    60 : 'sixty',
+    70 : 'seventy',
+    80 : 'eighty',
+    90 : 'ninety',
+    100 : 'hundred',
+    1000 : 'thousand',
+}
+
+def spell_number(n, words):
+    while True:
+        if n == 1000:
+            return words[1] + "" + words[1000]
+        elif n >= 100:
+            if n%100 == 0:
+                return words[n/100] + words[100]
+            else:
+                return words[n//100]+ words[100] + "and" + spell_number(n%100, words)
+        elif not n in words:
+            return words[(n//10)*10] + spell_number(n%10, words)
+        else:
+            return words[n]
+
+result = 0
+for i in range(1, 1001):
+    result += len(spell_number(i, words))
+
+print(result)
